0a4e0f2294b6bc7957ff09a06ece08e7b2a58e0e
Grzegorz Watroba <grzegorz.watroba@gmail.com>
2015-09-15 16:55:50 +0200

    added shelf from linux

---
+++ shelf/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+
+from .models import Author, Publisher, Book
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    search_fields = ['last_name','first_name']
+    ordering = ['last_name','first_name']
+
+
+class BookAdmin(admin.ModelAdmin):
+    search_fields = ['title']
+    list_display = ['title', 'author', 'isbn', 'publisher']
+
+admin.site.register(Book, BookAdmin)
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Publisher)
---
+++ shelf/migrations/0001_initial.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Author',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True, serialize=False)),
+                ('first_name', models.CharField(max_length=20)),
+                ('last_name', models.CharField(max_length=50)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Book',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True, serialize=False)),
+                ('title', models.CharField(max_length=100)),
+                ('isbn', models.CharField(max_length=17)),
+                ('author', models.ForeignKey(to='shelf.Author')),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Publisher',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True, serialize=False)),
+                ('name', models.CharField(max_length=70)),
+            ],
+        ),
+        migrations.AddField(
+            model_name='book',
+            name='publisher',
+            field=models.ForeignKey(to='shelf.Publisher'),
+        ),
+    ]
