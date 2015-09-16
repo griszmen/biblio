dc7b3b8321958414a5346f1aa46c8cc727d1c3bb
unknown <grzegorz.watroba@CAN20287-13.nsa.comarch>
2015-09-16 08:57:34 +0200

    code after 3rd meeting

---
+++ shelf/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import include, url
+from shelf.views import AuthorListView, AuthorDetailView
+
+urlpatterns = [
+    url(r'^autorzy/$', AuthorListView.as_view(), name='author-list'),
+    url(r'^autorzy/(?P<pk>\d+)/$', AuthorDetailView.as_view(), name='author-detail'),
+]
